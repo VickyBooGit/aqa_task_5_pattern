8b2b9855cb1b5966e757a64888f35739d4cf2144
Victoria Bukina <victoria.bukina@gmail.com>
2020-02-23 23:34:39 +0300

    New methods added.

--- src/test/java/CardDelivery.java
+++ src/test/java/CardDelivery.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.time.LocalDate;
 import java.util.Locale;
 
 import static com.codeborne.selenide.Selectors.withText;
@@ -22,26 +21,24 @@ public class CardDelivery {
     @Test
     void shouldScheduleDateOfCardDelivery() {
         open("http://localhost:9999");
-        $("[placeholder='Город']").setValue(faker.address().cityName()).pressEnter();
+        $("[placeholder='Город']").setValue(DataHelper.getNewCity());
         $("[type='tel']").sendKeys(Keys.CONTROL, "a");
         $("[type='tel']").sendKeys(Keys.DELETE);
-        $("[type='tel']").setValue(Utils.getFormattedRandomDate());
+        $("[type='tel']").setValue(DataHelper.getFormattedRandomDate());
 //      $("[type='tel']").setValue(String.valueOf(LocalDate.now().plusDays(5).getDayOfMonth() + "." + LocalDate.now().getMonthValue() + "." + LocalDate.now().getYear()));
-        $("[name='name']").setValue(String.valueOf(faker.name().lastName() + " " + faker.name().firstName()));
-        $("[name='phone']").setValue(String.valueOf(faker.phoneNumber().cellPhone()));
+        $("[name='name']").setValue(DataHelper.getNewName());
+        $("[name='phone']").setValue(DataHelper.getNewPhone());
         $("[class='checkbox__box']").click();
         $$("button").find(Condition.exactText("Запланировать")).click();
         $(withText("Успешно!")).waitUntil(Condition.visible, 25000);
         $("[type='tel']").sendKeys(Keys.CONTROL, "a");
         $("[type='tel']").sendKeys(Keys.DELETE);
-        $("[type='tel']").setValue(Utils.getFormattedRandomDate());
+        $("[type='tel']").setValue(DataHelper.getFormattedRandomDate());
         $$("button").find(Condition.exactText("Запланировать")).click();
         $(withText("Необходимо подтверждение")).waitUntil(Condition.visible, 25000);
         $$("button").find(Condition.exactText("Перепланировать")).click();
         $(withText("Успешно!")).waitUntil(Condition.visible, 25000);
     }
-
-
 
 
 }
